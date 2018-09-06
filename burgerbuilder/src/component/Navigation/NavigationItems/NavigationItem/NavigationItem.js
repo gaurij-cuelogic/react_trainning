e773a64b7855158e97afd039e63baeb1d176bcdf
gaurijoshi <gauri.joshi@cuelogic.com>
2018-09-06 19:44:57 +0530

    sidebar

--- burgerbuilder/src/component/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burgerbuilder/src/component/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,8 +3,7 @@ import classes from './NavigationItem.css'
 
 
 const navigationItem =(props) => (
-<li 
-className={classes.navigationItem}>
+<li className={classes.NavigationItem}>
 <a 
 href ={props.link} 
 className ={props.active ? classes.active :null}>{props.children}</a>
--- burgerbuilder/src/component/Navigation/NavigationItems/NavigationItems.js
+++ burgerbuilder/src/component/Navigation/NavigationItems/NavigationItems.js
@@ -3,7 +3,7 @@ import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = () => (
-    <ul className = {classes.navigationItems}>
+    <ul className = {classes.NavigationItems}>
        <NavigationItem link ="/" active ={true}>  Burger Builder </NavigationItem>
        <NavigationItem link ="/"> Checkout </NavigationItem>
     </ul>
